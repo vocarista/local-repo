9d695e27be208a375102e52a043a36d005d4ff16
Kumar Piyush <kr.piyush888@gmail.com>
2023-02-25 00:22:40 +0530

    updated test.js

--- test.js
+++ test.js
@@ -2,3 +2,6 @@ console.log('This is an output message');
 console.log('This is version 1.0');
 console.log('This is version 2.0');
 console .log('This is version 3.0')
+console.log('This change was made when master was being edited')
+
+
