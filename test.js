3c8289e6f2bd39fbb19df9698f3bf505f5ef43c8
Kumar Piyush <kr.piyush888@gmail.com>
2023-02-24 23:43:52 +0530

    created tags

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
 console.log('This is an output message');
 console.log('This is version 1.0');
 console.log('This is version 2.0');
+console .log('This is version 3.0')
--- test.py
+++ test.py
@@ -1,2 +1,3 @@
 print('This is version 1.0')
 print('This is version 2.0')
+print('This is version 3.0')
