142ee7d1bc5f09db4d79bed0c0c66d8d319ab7a6
Kumar Piyush <kr.piyush888@gmail.com>
2023-02-24 11:34:04 +0530

    Created version 2.0

--- test.js
+++ test.js
@@ -1,2 +1,3 @@
 console.log('This is an output message');
 console.log('This is version 1.0');
+console.log('This is version 2.0');
--- test.py
+++ test.py
@@ -1,1 +1,2 @@
 print('This is version 1.0')
+print('This is version 2.0')
